22fa099b329c8a22cf14516738b9807424e08dbd
saifuddin khan <109955734+khansaif1023@users.noreply.github.com>
2024-03-27 02:10:43 +0530

    some change

---
+++ src/main/java/com/myShopping/MyShopping/models/AppUser.java
@@ -0,0 +1,35 @@
+package com.myShopping.MyShopping.models;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+@Entity // This annotation will tell hibernate that you
+       // Need to create one table for this class
+@Table
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+
+    @Id // This annotation tell that id is unique
+    private UUID id; // unique base64Id--->It is the kind that got generated on the basisi of time
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+    private String password;
+    private String userType;
+    @Column(unique = true,length = 10)
+    private Long phoneNumber;
+
+}
