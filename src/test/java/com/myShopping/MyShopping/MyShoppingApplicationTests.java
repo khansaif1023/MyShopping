66c07174fae8613b79129eeb92121bddf53fd525
saifuddin khan <109955734+khansaif1023@users.noreply.github.com>
2024-03-26 23:54:04 +0530

    Shopping

---
+++ src/test/java/com/myShopping/MyShopping/MyShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myShopping.MyShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
